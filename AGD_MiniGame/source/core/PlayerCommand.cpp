ff26eae190388c800e89bc11df63fa69da2e5495
Sean-Sanii-Nejad <s.saniinejad@se22.qmul.ac.uk>
2023-02-25 13:58:19 +0000

    Attacking/Shouting Animation fix

--- AGD_MiniGame/source/core/PlayerCommand.cpp
+++ AGD_MiniGame/source/core/PlayerCommand.cpp
@@ -4,30 +4,36 @@
 
 void MoveRightCommand::execute(Game& game)
 {
+	if (game.getPlayer()->isAttacking() || game.getPlayer()->isShouting()) return;
 	game.getPlayer()->setVelocityX(1.f);
 }
 
 void MoveLeftCommand::execute(Game& game)
 {
+	if (game.getPlayer()->isAttacking() || game.getPlayer()->isShouting()) return;
 	game.getPlayer()->setVelocityX(-1.f);
 }
 
 void MoveUpCommand::execute(Game& game)
 {
+	if (game.getPlayer()->isAttacking() || game.getPlayer()->isShouting()) return;
 	game.getPlayer()->setVelocityY(-1.f);
 }
 
 void MoveDownCommand::execute(Game& game)
 {
+	if (game.getPlayer()->isAttacking() || game.getPlayer()->isShouting()) return;
 	game.getPlayer()->setVelocityY(1.f);
 }
 
 void AttackCommand::execute(Game& game)
 {
+	if (game.getPlayer()->isAttacking() || game.getPlayer()->isShouting()) return;
 	game.getPlayer()->setAttacking(true);
 }
 
 void FireCommand::execute(Game& game)
 {
+	if (game.getPlayer()->isAttacking() || game.getPlayer()->isShouting()) return;
 	game.getPlayer()->setShouting(true);
 }
