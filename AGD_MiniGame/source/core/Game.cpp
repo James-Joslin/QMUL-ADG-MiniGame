b74757fd058175313dd9d6817978ac418a4a3666
James Mark Joslin <j.m.joslin@se22.qmul.ac.uk>
2023-05-03 21:08:27 +0000

    reanbled archetypes after testing big array still worked

--- AGD_MiniGame/source/core/Game.cpp
+++ AGD_MiniGame/source/core/Game.cpp
@@ -11,7 +11,7 @@
 
 
 // III.F Add the initialization (to 0) of the entity counter to the initalizers list of this constructor
-Game::Game() : paused(false), drawDebug(true), id{ 0 }, useArchetypes(false)
+Game::Game() : paused(false), drawDebug(true), id{ 0 }, useArchetypes(true)
 {
 	if (!useArchetypes) // if not using archetypes use big array and add systems to logic and graphics system vectors
 	{
